e6a8eccac03d393effa58d00238c3072749d9507
huyz1117 <huyz@cumt.edu.cn>
2018-11-25 22:22:36 +0800

    add tensorboard

--- AlexNet.py
+++ AlexNet.py
@@ -1,152 +1,150 @@
 # Copyright (c) 2018 by huyz. All Rights Reserved.
 
-"""
-AlexNet采用的结构如下：
-1、Relu：标准CNN采用激活函数tanh,但进行梯度下降计算时，Relu函数训练速度更快
-2、Local Response Normalization：局部归一化，在Relu之后采用，主要时为了提高模型泛化能力，现在通常采用BN
-3、Overlapping Pool：重叠池化，传统CNN的pool层，ksize与stride相同，但论文发现，当stride<ksize时，
-                    模型更难过拟合，所以论文采用ksize=3,stride=2
-4、Dropout：Dropout层采用神经元丢弃策略，主要时为了避免模型过拟合，失活的神经元不再参与前向传播与反向传播
-"""
-
 
 import tensorflow as tf
+
 from tensorflow.examples.tutorials.mnist import input_data
-
-mnist = input_data.read_data_sets("MNIST_data", one_hot=True)
-
-#X_train = mnist.train.images
-#print(X_train.shape)
-
-X = tf.placeholder(tf.float32, shape=[None, 784])
-X_img = tf.reshape(X, shape=[-1, 28, 28, 1])
-Y = tf.placeholder(tf.float32, shape=[None, 10])
+mnist = input_data.read_data_sets("MNIST", one_hot=True)
 
 learning_rate = 0.001
-num_epochs = 20
 batch_size = 128
+num_epochs = 2
 dropout = 0.5
 
-######################## 定义卷积 ############################
-def conv2d(name, x, W, b, strides=1, padding="SAME"):
-    x = tf.nn.conv2d(x, W, strides=[1, strides, strides, 1], padding=padding)
-    x = tf.nn.bias_add(x, b)
-    return tf.nn.relu(x, name=name)
+X = tf.placeholder(tf.float32, shape=[None, 784])
+X_img = tf.reshape(X, [-1, 28, 28, 1])
+Y = tf.placeholder(tf.float32, shape=[None, 10])
 
-####################### 定义池化 #############################
-def maxpool(name, x, k=3, s=2, padding="SAME"):
-    return tf.nn.max_pool(x, ksize=[1, k, k, 1], strides=[1, s, s, 1],
-                        padding=padding, name=name)
+with tf.variable_scope("conv1"):
+    W1 = tf.get_variable(name="W1", shape=[11, 11, 1, 96], initializer=tf.truncated_normal_initializer())
+    b1 = tf.get_variable(name="b1", shape=[96], initializer=tf.zeros_initializer())
+    W1_hist = tf.summary.histogram("W1", W1)
+    b1_hist = tf.summary.histogram("b1", b1)
+    conv1 = tf.nn.conv2d(X_img, W1, strides=[1, 1, 1, 1], padding="SAME")
+    #conv1 = tf.layers.conv2d()
+    conv1 = tf.nn.bias_add(conv1, b1)
+    conv1 = tf.nn.relu(conv1)
+    conv1_hist = tf.summary.histogram("conv1", conv1)
 
-####################### 定义全连接 ############################
-def fc(x, W, b):
-    return tf.matmul(x, W) + b
+with tf.variable_scope("pool1"):
+    pool1 = tf.nn.max_pool(conv1, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding="SAME")
 
-# 规范化
-def lrn(name, x, lsize=5):
-    return tf.nn.local_response_normalization(x, lsize, bias=1.0, alpha=0.0001,
-                        beta=0.75, name=name)
+with tf.variable_scope("lrn1"):
+    lrn1 = tf.nn.local_response_normalization(pool1, 5, bias=1.0,
+                                            alpha=0.0001, beta=0.75)
 
-# 卷积核的参数
-def weight(shape, mean=0.0, stddev=0.1):
-    return tf.Variable(tf.truncated_normal(shape=shape, mean=mean, stddev=stddev))
+with tf.variable_scope("conv2"):
+    W2 = tf.get_variable(name="W2", shape=[5, 5, 96, 256], initializer=tf.truncated_normal_initializer())
+    b2 = tf.get_variable(name="b2", shape=[256], initializer=tf.zeros_initializer())
+    W2_hist = tf.summary.histogram("W1", W2)
+    b2_hist = tf.summary.histogram("b2", b2)
+    conv2 = tf.nn.conv2d(lrn1, W2, strides=[1, 1, 1, 1], padding="SAME")
+    conv2 = tf.nn.bias_add(conv2, b2)
+    conv2 = tf.nn.relu(conv2)
+    conv2_hist = tf.summary.histogram("conv2", conv2)
 
-def bias(shape):
-    return tf.Variable(tf.zeros(shape=shape))
+with tf.variable_scope("pool2"):
+    pool2 = tf.nn.max_pool(conv2, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding="SAME")
 
-weights = {"W1": weight([11, 11, 1, 96]),
-            "W2": weight([5, 5, 96, 256]),
-            "W3": weight([3, 3, 256, 384]),
-            "W4": weight([3, 3, 384, 384]),
-            "W5": weight([3, 3, 384, 256]),
-            "W6": weight([4*4*256, 4096]),
-            "W7": weight([4096, 4096]),
-            "W8": weight([4096, 10])}
+with tf.variable_scope("lrn2"):
+    lrn2 = tf.nn.local_response_normalization(pool2, 5, bias=1.0,
+                                            alpha=0.0001, beta=0.75)
 
-bias = {"b1": bias([96]),
-        "b2": bias([256]),
-        "b3": bias([384]),
-        "b4": bias([384]),
-        "b5": bias([256]),
-        "b6": bias([4096]),
-        "b7": bias([4096]),
-        "b8": bias([10])}
+with tf.variable_scope("conv3"):
+    W3 = tf.get_variable(name="W3", shape=[3, 3, 256, 384], initializer=tf.truncated_normal_initializer())
+    b3 = tf.get_variable(name="b3", shape=[384], initializer=tf.truncated_normal_initializer())
+    W3_hist = tf.summary.histogram("W3", W3)
+    b3_hist = tf.summary.histogram("b3", b3)
+    conv3 = tf.nn.conv2d(lrn2, W3, strides=[1, 1, 1, 1], padding="SAME")
+    conv3 = tf.nn.bias_add(conv3, b3)
+    conv3 = tf.nn.relu(conv3)
+    conv3_hist = tf.summary.histogram("conv3", conv3)
 
-def alexnet(x, weight, bias, dropout):
+with tf.variable_scope("conv4"):
+    W4 = tf.get_variable(name="W4", shape=[3, 3, 384, 384], initializer=tf.truncated_normal_initializer())
+    b4 = tf.get_variable(name="b4", shape=[384], initializer=tf.zeros_initializer())
+    W4_hist = tf.summary.histogram("W4", W4)
+    b4_hist = tf.summary.histogram("b4", b4)
+    conv4 = tf.nn.conv2d(conv3, W4, strides=[1, 1, 1, 1], padding="SAME")
+    conv4 = tf.nn.bias_add(conv4, b4)
+    conv4 = tf.nn.relu(conv4)
+    conv4_hist = tf.summary.histogram("conv4", conv4)
 
-    ### 第1层卷积 ###
-    ## conv size: 28*28*2 --> 28*28*96   ceil(28/1) ##
-    conv1 = conv2d("conv1", x, weights["W1"], bias["b1"], padding="SAME")
-    ## pool size: 28*28*96 --> 14*14*96  ceil(28/2) ##
-    pool1 = maxpool("pool1", conv1, k=3, s=2, padding="SAME")
-    ## lrn size: 14*14*96 --> 14*14*96  ##
-    lrn1 = lrn("lrn1", pool1, lsize=5)
+with tf.variable_scope("conv5"):
+    W5 = tf.get_variable(name="W5", shape=[3, 3, 384, 256], initializer=tf.truncated_normal_initializer())
+    b5 = tf.get_variable(name="b5", shape=[256], initializer=tf.zeros_initializer())
+    W5_hist = tf.summary.histogram("W5", W5)
+    b5_hist = tf.summary.histogram("b5", b5)
+    conv5 = tf.nn.conv2d(conv4, W5, strides=[1, 1, 1, 1], padding="SAME")
+    conv5 = tf.nn.bias_add(conv5, b5)
+    conv5 = tf.nn.relu(conv5)
+    conv5_hist = tf.summary.histogram("W1", conv5)
 
-    ### 第2层卷积 ###
-    ## conv size: 14*14*96 --> 14*14*256  ceil(14/1) ##
-    conv2 = conv2d("conv2", lrn1, weights["W2"], bias["b2"], padding="SAME")
-    ## pool size: 14*14*256 --> 7*7*256  ceil(14/2) ##
-    pool2 = maxpool("pool2", conv2, k=3, s=2, padding="SAME")
-    ## lrn size: 7*7*256 --> 7*7*256
-    lrn2 = lrn("lrn2", pool2, lsize=5)
+with tf.variable_scope("pool5"):
+    pool5 = tf.nn.max_pool(conv5, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding="SAME")
 
-    ### 第3层卷积 ###
-    ## conv size: 7*7*256 --> 7*7*384 ceil(7/1) ##
-    conv3 = conv2d("conv3", lrn2, weights["W3"], bias["b3"], padding="SAME")
+with tf.variable_scope("lrn5"):
+    lrn5 = tf.nn.local_response_normalization(pool5, 5, bias=1.0,
+                                            alpha=0.0001, beta=0.75)
 
-    ### 第4层卷积 ###
-    ## conv size: 7*7*384 --> 7*7*384 ceil(7/1) ##
-    conv4 = conv2d("conv4", conv3, weights["W4"], bias["b4"], padding="SAME")
-
-    ### 第5层卷积 ###
-    ## conv size: 7*7*384 --> 7*7*256 ceil(7/1) ##
-    conv5 = conv2d("conv4", conv4, weights["W5"], bias["b5"], padding="SAME")
-    ## pool size: 7*7*256 --> 4*4*256 ceil(7/2) ##
-    pool5 = maxpool("pool5", conv5, k=3, s=2, padding="SAME")
-    ## lrn size: 4*4256 --> 4*4*256 ##
-    lrn5 = lrn("lrn5", pool5, lsize=5)
-
-    ### 第1个全连接层 ###
-    ## fc1 size: 4*4*256 --> 4096
-    fc1 = tf.reshape(lrn5, shape=[-1, 4*4*256])
-    fc1 = fc(fc1, weights["W6"], bias["b6"])
+with tf.variable_scope("fc1"):
+    flatten = tf.reshape(lrn5, shape=[-1, 4*4*256])
+    W6 = tf.get_variable(name="W6", shape=[4*4*256, 4096], initializer=tf.truncated_normal_initializer())
+    b6 = tf.get_variable(name="b6", shape=[4096], initializer=tf.zeros_initializer())
+    W6_hist = tf.summary.histogram("W6", W6)
+    b6_hist = tf.summary.histogram("b6", b6)
+    fc1 = tf.matmul(flatten, W6) + b6
     fc1 = tf.nn.relu(fc1)
-    ## dropout
-    fc1 = tf.nn.dropout(fc1, dropout)
+    fc1 = tf.nn.dropout(fc1, keep_prob=dropout)
+    fc1_hist = tf.summary.histogram("W1", fc1)
 
-    ### 第2个全连接层 ###
-    ## fc2 size: 4096 --> 4096 ##
-    fc2 = fc(fc1, weights["W7"], bias["b7"])
+
+with tf.variable_scope("fc2"):
+    W7 = tf.get_variable(name="W7", shape=[4096, 4096], initializer=tf.truncated_normal_initializer())
+    b7 = tf.get_variable(name="b7", shape=[4096], initializer=tf.zeros_initializer())
+    W7_hist = tf.summary.histogram("W7", W7)
+    b7_hist = tf.summary.histogram("b7", b7)
+    fc2 = tf.matmul(fc1, W7) + b7
     fc2 = tf.nn.relu(fc2)
-    # dropout
-    fc2 = tf.nn.dropout(fc2, dropout)
+    fc2 = tf.nn.dropout(fc2, keep_prob=dropout)
+    fc2_hist = tf.summary.histogram("fc2", fc2)
 
-    ### 第3个全连接层 ###
-    ## fc3 size: 4096 --> 10 ##
-    logit = fc(fc2, weights["W8"], bias["b8"])
+with tf.variable_scope("fc3"):
+    W8 = tf.get_variable(name="W8", shape=[4096, 10], initializer=tf.truncated_normal_initializer())
+    b8 = tf.get_variable(name="b8", shape=[10], initializer=tf.zeros_initializer())
+    W8_hist = tf.summary.histogram("W8", W8)
+    b8_hist = tf.summary.histogram("b8", b8)
+    logit = tf.matmul(fc2, W8) + b8
+    logit_hist = tf.summary.histogram("logit", logit)
 
-    return logit
+# cost/loss function
+with tf.variable_scope("cost"):
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logit, labels=Y))
+    cost_summ = tf.summary.scalar("loss", cost)
 
-########### 定义模型、损失函数、优化函数 ##############
+with tf.variable_scope("train"):
+    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
 
-### 定义模型 ###
-pred = alexnet(X_img, weight, bias, dropout)
-
-### 损失函数 ###
-cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=Y))
-optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
-
-### 准确率 ###
-is_correction= tf.equal(tf.argmax(Y, 1), tf.argmax(pred, 1))
+# 准确率
+is_correction= tf.equal(tf.argmax(Y, 1), tf.argmax(logit, 1))
 accuracy = tf.reduce_mean(tf.cast(is_correction, tf.float32))
+acc_summ = tf.summary.scalar("accuracy", accuracy)
+
+# summary
+summary = tf.summary.merge_all()
 
 with tf.Session() as sess:
     init = tf.global_variables_initializer()
     sess.run(init)
 
+    # 创建summary writer
+    writer = tf.summary.FileWriter("./log")
+    writer.add_graph(sess.graph)
+
+
     print("Learning start...")
 
-    for epoch in range(num_epochs+1):
+    for epoch in range(num_epochs):
         avg_acc = 0
         avg_cost = 0
         num_batches = int(mnist.train.num_examples / batch_size)
@@ -154,8 +152,9 @@ with tf.Session() as sess:
         feed_dict = {X: batch_x, Y: batch_y}
 
         for i in range(num_batches):
-            _, c, a = sess.run([optimizer, cost, accuracy], feed_dict=feed_dict)
+            s, _, c, a = sess.run([summary, optimizer, cost, accuracy], feed_dict=feed_dict)
+            writer.add_summary(s, global_step=i)
             avg_acc += a / num_batches
             avg_cost += c / num_batches
-        print("Epoch: {}\tLoss: {}\t Accuracy: {:.3%}".format(epoch+1, avg_cost, avg_acc))
+        print("Epoch: {}\tLoss: {:.9f}\tAccuracy: {:.3%}".format(epoch+1, avg_cost, avg_acc))
     print("Learning finished!")
